77fa5865152d47bed5766563b023c5129253169f
Benjamin <lenjaminbiu@gmail.com>
2020-09-26 15:48:19 +1000

    Updates

---
+++ precache-manifest.b6f7225fed229df2a472b46225511a0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39782c3b54e1c0feaf901340ec241d40",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f44b8fff20e444a5276c",
+    "url": "/monsters-rolodex/static/css/main.f2f76393.chunk.css"
+  },
+  {
+    "revision": "0414063dac2cacd9051a",
+    "url": "/monsters-rolodex/static/js/2.ff5662b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.ff5662b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f44b8fff20e444a5276c",
+    "url": "/monsters-rolodex/static/js/main.e03ffa54.chunk.js"
+  },
+  {
+    "revision": "439aac3230854f58a9fc",
+    "url": "/monsters-rolodex/static/js/runtime-main.bca7ff1c.js"
+  }
+]);
